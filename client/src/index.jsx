807f71ebdd431e8c0d9ffafd4712e2b46852197a
Robert <robert.pakkanen@helsinki.fi>
2020-01-14 21:42:51 +0200

    now displaying list of packages

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-const element = <h1>Hello World</h1>;
-ReactDOM.render(element, document.getElementById('root'));
+const App = () => {
+  const [packages, setPackages] = useState([]);
+
+  useEffect(() => {
+    const getPackages = async () => {
+      const response = await axios.get('http://localhost:3001/api/packages');
+      setPackages(response.data);
+    };
+    getPackages();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {packages.map(pkg => (
+          <li key={pkg}>{pkg}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
