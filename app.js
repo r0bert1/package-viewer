d4b587643f30885b27c23f6c0074610c2f7c6ecd
Robert <robert.pakkanen@helsinki.fi>
2020-01-14 02:39:05 +0200

    added linting

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-const fs = require('fs')
-const readline = require('readline')
-const http = require('http')
+const fs = require('fs');
+const readline = require('readline');
+const http = require('http');
 
-let packages = []
+const packages = [];
 
-readline.createInterface({
-  input: fs.createReadStream('status.real'),
-  terminal: false
-}).on('line', (line) => {
-  if (line.startsWith('Package:')) {
-    packages.push(line.slice(9))
-  }
-})
+readline
+  .createInterface({
+    input: fs.createReadStream('status.real'),
+    terminal: false
+  })
+  .on('line', line => {
+    if (line.startsWith('Package:')) {
+      packages.push(line.slice(9));
+    }
+  });
 
 const server = http.createServer((request, response) => {
   if (request.url === '/') {
-    const sortedPackages = packages.sort()
-    response.write(JSON.stringify(sortedPackages))
-    response.end()
+    const sortedPackages = packages.sort();
+    response.write(JSON.stringify(sortedPackages));
+    response.end();
   }
-})
+});
 
-server.listen(3000)
+server.listen(3000);
