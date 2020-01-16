e3c6eadcf6a89521c69cc3b6fd8dbe523e987ece
Robert <robert.pakkanen@helsinki.fi>
2020-01-16 15:01:31 +0200

    now displaying hardcoded description

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -13,13 +13,32 @@ const App = () => {
     getPackages();
   }, []);
 
+  const findPackage = name => packages.find(pkg => pkg.name === name);
+
+  const columnStyle = {
+    display: 'flex'
+  };
+
+  const itemStyle = {
+    flex: '1'
+  };
+
   return (
-    <div>
-      <ul>
-        {packages.map(pkg => (
-          <li key={pkg}>{pkg}</li>
-        ))}
-      </ul>
+    <div style={columnStyle}>
+      <div style={itemStyle}>
+        <ul>
+          {packages.map(pkg => (
+            <li key={pkg.name}>{pkg.name}</li>
+          ))}
+        </ul>
+      </div>
+      {packages.length > 0 ? (
+        <div style={itemStyle}>
+          <p>{findPackage('accountsservice').desc}</p>
+        </div>
+      ) : (
+        <div />
+      )}
     </div>
   );
 };
