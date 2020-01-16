440b8c867e302c69a4140b9f6fef9dc348bd8e6a
Robert <robert.pakkanen@helsinki.fi>
2020-01-16 14:59:55 +0200

    added description to response

--- server/app.js
+++ server/app.js
@@ -15,12 +15,26 @@ readline
   })
   .on('line', line => {
     if (line.startsWith('Package:')) {
-      packages.push(line.slice(9));
+      packages.push({ name: line.slice(9) });
+    } else if (line.startsWith('Description:')) {
+      packages[packages.length - 1].desc = line.slice(13);
+    } else if (line.startsWith(' ')) {
+      packages[packages.length - 1].desc += line;
     }
   });
 
 app.get('/api/packages', (req, res) => {
-  res.send(packages.sort());
+  res.send(
+    packages.sort((pkg1, pkg2) => {
+      if (pkg1.name < pkg2.name) {
+        return -1;
+      }
+      if (pkg1.name > pkg2.name) {
+        return 1;
+      }
+      return 0;
+    })
+  );
 });
 
 app.listen(3001);
