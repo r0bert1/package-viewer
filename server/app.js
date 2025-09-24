47af8eb1a548f35d550d26d4163549bdc3a43372
Robert Pakkanen <rpakkane@gmail.com>
2025-09-24 09:47:35 +0300

    Set up deployment pipeline

--- server/app.js
+++ server/app.js
@@ -3,11 +3,21 @@ const cors = require('cors');
 const { processPackages } = require('./utils/packageHelper');
 
 const app = express();
+
+const PORT = process.env.PORT || 3001;
+
 app.use(cors());
+app.use(express.static('client/build'));
+
+app.get('/health', (req, res) => {
+  res.send('ok');
+});
 
 app.get('/api/packages', async (req, res) => {
   const packages = await processPackages();
   res.send(packages);
 });
 
-app.listen(3001);
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
